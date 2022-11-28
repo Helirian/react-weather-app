80d7f6d3a0768c2f3e71ce005105bda78ff60ef8
Helirian <omoniieva@gmail.com>
2022-11-28 23:35:06 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,37 @@ function App() {
       <header className="App-header">
         <h1>Weather Search Engine</h1>
         <Weather />
+        <footer>
+          <small>
+            Project made by
+            <a
+              href="https://github.com/Helirian"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              Helirian
+            </a>{" "}
+            and is
+            <a
+              href="https://github.com/Helirian/vanilla-weather-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              open-sorced on GitHub
+            </a>{" "}
+            and
+            <a
+              href="https://cozy-buttercream-b60c79.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              hosted on Netlify
+            </a>
+          </small>
+        </footer>
       </header>
     </div>
   );
